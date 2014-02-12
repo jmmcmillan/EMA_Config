b58c7ee31d9d8729f81cf547541729821be304ca
J. McMillan <j.m.mcmillan@gmail.com>
2014-02-12 17:49:34 -0500

    Slider response type

---
+++ EMA Configuration Tool/Model/Responses/Slider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMA_Configuration_Tool.Model.Responses
+{
+    public class Slider : StringChoice
+    {
+        public override string ResponseXMLType
+        { 
+            get 
+            {
+                return "Slider"; 
+            } 
+        }
+
+        public Slider()
+            : base()
+        {
+            
+        }
+
+        public override ResponseBase Copy()
+        {
+            Slider slide = new Slider();
+            slide.Responses = Responses;
+            return slide;
+        }
+    }
+}
