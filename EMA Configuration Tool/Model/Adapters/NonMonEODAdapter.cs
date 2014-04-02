aab76dea26bcbb9ecfc059b61cc377283dded977
J. McMillan <j.m.mcmillan@gmail.com>
2014-04-02 16:47:30 -0400

    Removed instructions about pressing actiwatch buttons from EOD content adapters

--- EMA Configuration Tool/Model/Adapters/NonMonEODAdapter.cs
+++ EMA Configuration Tool/Model/Adapters/NonMonEODAdapter.cs
@@ -17,7 +17,7 @@ namespace EMA_Configuration_Tool.Model.Adapters
         {
             Question finalPrompt = new Question();
             finalPrompt.Label = "p";
-            finalPrompt.Text = "1. Take off Actical from your belt. Press the * button.\n\n2. Please press the Actiwatch button when you are trying to go to sleep.\n\n3. Please plug the ED in NOW so that it may charge overnight!";
+            finalPrompt.Text = "1. Take off Actical from your belt. Press the * button.\n\n2. Please keep the Actiwatch on.\n\n3. Please plug the ED in NOW so that it may charge overnight!";
             finalPrompt.Response = new EMA_Configuration_Tool.Model.Responses.Prompt();
 
             PostQuestions.Add(finalPrompt);
