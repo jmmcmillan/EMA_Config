30196c00d8a876b74b31a4040ca765c71c27d0c2
J. McMillan <j.m.mcmillan@gmail.com>
2016-03-17 00:44:03 -0400

    Adding FillIn response element

---
+++ EMA Configuration Tool/Model/Responses/FillIn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMA_Configuration_Tool.Model.Responses
+{
+    public class FillIn : ResponseBase
+    {
+        public override string ResponseXMLType
+        {
+            get
+            {
+                return "FillIn";
+            }
+        }
+
+        public FillIn() : base() { }
+
+        public override ResponseBase Copy()
+        {
+            return new FillIn();
+        }
+    }
+}
